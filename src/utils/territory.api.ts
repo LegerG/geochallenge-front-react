a6449d633f07057a0642831c7128cc07fcbfc3f1
Gwenaël Léger <gwenaelleger@gmail.com>
2022-10-30 14:02:29 +0100

    Select the united nation group for game

--- src/utils/territory.api.ts
+++ src/utils/territory.api.ts
@@ -29,7 +29,7 @@ export const territoryApi = createApi({
       query: () => ({
         method: "GET",
         url: `/api/territory`,
-        params: { size: 50 },
+        params: { size: 50, group: "un" },
       }),
     }),
     getTrainingGame: builder.query<Territory[], CountryParams>({
